7fbba26ee3b16926b59fb0c35f4ad341530fd0c1
jams032 <jamshed.bas@gmail.com>
2021-01-26 15:02:32 +0600

    Unit test-ongoing
    Unit test-ongoing

--- SpaceX_API/SpaceX_APITests/LaunchesViewModelTests.swift
+++ SpaceX_API/SpaceX_APITests/LaunchesViewModelTests.swift
@@ -44,27 +44,27 @@ class LaunchesViewModelTests: XCTestCase {
     
     func testInit() {
         XCTAssert(nil != self.launchesViewModel)
-       // XCTAssert(nil != self.launchesViewModel?.wordList)
     }
     
     func testLaunchModel() {
 
+
         let lightModel1 = LaunchModel(links: (Links(patch: (Patch(small: "url", large: "url")), wikipedia: "wiki")), staticFireDateUTC: "2020", staticFireDateUnix: 20202, rocket: "scddcd334", success: true, details: "Hello details", launchpad: "launch pad", autoUpdate: true, flightNumber: 110, name: "Falcon9", dateUTC1: Date.init(), launchDate: "2020", dateUnix: 22424, datePrecision: "2202", upcoming: true, id: "123")
-          
-          
-        guard let viewModel = self.launchesViewModel else { return XCTFail() }
-
-
         
-      //  XCTAssert(lightModel1.links?.patch?.small?.count > 0 , "No Image links comes")
+
+        XCTAssert((lightModel1.links?.patch?.small!.count)! > 0 , "No Image links comes")
+        XCTAssert((lightModel1.id!.count) > 0 , "No ID comes")
+        XCTAssert((lightModel1.links?.wikipedia!.count)! > 0 , "No wikipedia links comes")
+        XCTAssert(lightModel1.name.count > 0 , "No name links comes")
+        XCTAssert(lightModel1.launchDate.count > 0 , "No launchDate links comes")
+
         
         
         let disposeBag = DisposeBag()
-
-        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = Date.localFormat
         var tableView = UITableView()
+        guard let viewModel = self.launchesViewModel else { return XCTFail() }
 
         let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, LaunchModel>>(
             configureCell: { dataSource, table, indexPath, item in
@@ -83,29 +83,62 @@ class LaunchesViewModelTests: XCTestCase {
         .bind(to: tableView.rx.items(dataSource: dataSource))
         .disposed(by: disposeBag)
         
-        viewModel.retrieveLaunches()
-
         
+        if let filePath = Bundle.main.path(forResource: "Launches", ofType: "json"), let data = NSData(contentsOfFile: filePath) {
+            do {
+                
+                let json = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments)
+                
+                let decoder = JSONDecoder()
+                var launches = try decoder.decode([LaunchModel].self, from: data as Data)
+                print(launches.first?.name)
+                
+            }
+            catch {
+                //Handle error
+                print("Error:", error.localizedDescription)
+            }
+        }
+        
+        guard let data = getJSONData(fileName: "Launches")else { return XCTFail() }
+
+        let decoder = JSONDecoder()
+            
+        let launches: [LaunchModel] = try! JSONDecoder().decode([LaunchModel].self, from:  data as Data)
+        print(launches.first?.name)
+
+
+        XCTAssertNotNil(launches.count == 0, "No Launches Found!")
+        XCTAssertNotNil(launches.first?.name.count == 0, "name Not Found!")
+        XCTAssertTrue(launches.first?.links?.patch?.small?.count == 0, "patch small name Not Found!")
+
       //  let fetchedElement = viewModel.getElement(at: IndexPath(row: 0, section: 0))
       //  XCTAssertEqual(element, fetchedElement)
         
-        
-      //  viewModel.deleteWord(index: 0)
-       // guard let listRet = viewModel.wordList else { return XCTFail() }
-       // XCTAssert(1 == listRet.value.count)
-        
-        //let vocaRet = listRet.value[0]
-       // XCTAssert("TEST2" == vocaRet.word && "TEST2_Meaning" == vocaRet.meaning)
     }
     
-    func testButtonEditPressed() {
-//        launchesViewModel?.isTableViewEditing.accept(false)
-//        launchesViewModel?.action.onNext(.buttonEditTapped)
-//        XCTAssert(true == launchesViewModel?.isTableViewEditing.value)
-//        XCTAssert("Edit" == launchesViewModel?.titleOfButtonEdit.value)
+    func testabc() {
+
+    }
+    
+    func getJSONData(fileName:String) -> Data? {
+        if let filePath = Bundle.main.path(forResource: fileName, ofType: "json"), let data = NSData(contentsOfFile: filePath) {
+            do {
+                
+//                let json = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments)
 //
-//        launchesViewModel?.action.onNext(.buttonEditTapped)
-//        XCTAssert(false == launchesViewModel?.isTableViewEditing.value)
-//        XCTAssert("Done" == launchesViewModel?.titleOfButtonEdit.value)
+//                let decoder = JSONDecoder()
+//                var launches = try decoder.decode([LaunchModel].self, from: data as Data)
+//                print(launches.first?.name)
+                return data as Data
+            }
+            catch {
+                //Handle error
+                print("Error:", error.localizedDescription)
+                return nil
+            }
+        }
+        return nil
+
     }
 }
